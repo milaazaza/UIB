b0fd037e7d2d63067eef60d3b22950c1bdfa4802
Camila Coelho Salazar <1192304656@acd.grupotiradentes.com>
2020-02-10 19:06:03 -0300

    le o nome

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class Hello {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("digite seu nome");
+		
+		String nome = sc.next();
+		
+		System.out.println(nome);
+		
+		sc.close();
 
 	}
 
